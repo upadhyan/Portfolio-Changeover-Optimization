f39dd5f794678a62a7f146e5cf450d19b949282e
Nakul Upadhya <upadhyan@purdue.edu>
2023-04-07 09:51:04 -0400

    update forecasting method

--- experimental_config.py
+++ experimental_config.py
@@ -1,31 +1,29 @@
 import pandas as pd
 import numpy as np
-
 from darts import TimeSeries
-from darts.models import NBEATSModel, TFTModel, NLinearModel, DLinearModel, NHiTSModel
+from darts.models import NHiTSModel
 from darts.metrics import mape
-import matplotlib.pyplot as plt
-import seaborn as sns
+from darts.utils.losses import MapeLoss
 import pickle
-
 import torch
+import random
+from tqdm import trange
+import os
+import logging
+import gc
 
 torch.set_float32_matmul_precision('medium')
-
-import random
-
-from tqdm import trange
-
-import os
-
-import logging
-
 logging.getLogger("pytorch_lightning.utilities.rank_zero").setLevel(logging.WARNING)
 logging.getLogger("pytorch_lightning.accelerators.cuda").setLevel(logging.WARNING)
 
 
-class Experiment:
-    def __init__(self, stock_prices: pd.DataFrame, covariates: pd.DataFrame):
+# Create a custom exception to handle errors
+class ExperimentGenerationError(Exception):
+    pass
+
+
+class ExperimentInfo:
+    def __init__(self, stock_prices: pd.DataFrame, covariates: pd.DataFrame, lookback):
         self.num_stocks = None
         self.stocks = None
         self.pct_variance = None
@@ -42,9 +40,9 @@ class Experiment:
         self.target_stock = None
         self.initial_portfolio_value = None
         self.final_portfolio_value = None
-        stocks = [x for x in stock_prices if x not in covariates]
+        self.lookback = lookback
+        self.full_trading_times = None
         self.generate_experiment(stock_prices, covariates)
-
 
     # Make a function that accepts a list as an input
     def set_stocks(self, stocks: list):
@@ -81,7 +79,7 @@ class Experiment:
         forbidden_stock = random.randint(0, self.num_stocks - 1)
         self.target_stock = forbidden_stock
         # Set self.initial_portfolio to a random portfolio of stocks
-        while (self.initial_portfolio + add) @ (self.initial_prices) < self.budget:
+        while (self.initial_portfolio + add) @ self.initial_prices < self.budget:
             # Make a random entry in add equal to 1
             self.initial_portfolio = self.initial_portfolio + add
             add[random.randint(0, self.num_stocks - 1)] = 1
@@ -89,7 +87,7 @@ class Experiment:
         add = np.zeros(self.num_stocks)
         self.initial_portfolio_value = self.initial_portfolio @ self.initial_prices
         # Create a target portfolio
-        while (self.final_portfolio + add) @ (self.initial_prices) < self.initial_portfolio_value:
+        while (self.final_portfolio + add) @ self.initial_prices < self.initial_portfolio_value:
             self.final_portfolio = self.final_portfolio + add
             add[random.randint(0, self.num_stocks - 1)] = 1
             temp_final = self.final_portfolio + add
@@ -101,8 +99,8 @@ class Experiment:
     def generate_experiment(self, stock_prices: pd.DataFrame, covariates: pd.DataFrame):
         random_date = self.choose_split_date()
         # get the date two years before random date
-        start_date = random_date - pd.DateOffset(years=4)
-        cov_start_date = random_date - pd.DateOffset(years=5)
+        start_date = random_date - pd.DateOffset(years=5)
+        cov_start_date = random_date - pd.DateOffset(years=6)
         # get the date 30 business days after random date
         end_date = random_date + pd.DateOffset(days=30)
         cov_end_date = random_date + pd.DateOffset(days=90)
@@ -124,61 +122,71 @@ class Experiment:
         subset = subset[stocks]
         full_idx = pd.date_range(start_date, end_date, freq='B')
         new_index = pd.date_range(cov_start_date, cov_end_date, freq='B')
-        # reindex the subset with business days and set the index name to 'date'
+
+        # Reindex the covariates
+        covariates = covariates.reindex(new_index, method='ffill')
+        # copy the subset
+        subset_copy = subset.copy()
+        # Rename the subset columns just in case
+        subset_copy.columns = [f'{col}_price' for col in subset_copy.columns]
+        # Add in the subset
+        covariates = covariates.join(subset_copy, how='left')
+        covariates = covariates.fillna(method='bfill')
+        # Set the index name to 'date'
+        covariates.index.name = 'date'
+        # Drop all columns with nans
+        covariates = covariates.dropna(axis=1, how='any')
+        # Get the list of covariates
+        cov_names = covariates.columns.tolist()
+        # Reset the index to create a date column
+        covariates = covariates.reset_index()
+        # Create a TimeSeries object from the covariates
+        cov_ts = TimeSeries.from_dataframe(covariates, time_col='date', value_cols=cov_names)
+
+        # Reindex the subset
         subset = subset.reindex(full_idx, method='ffill')
         subset.index.name = 'date'
 
+        # Get the date 30 trading periods before the end
         split_date = subset.index[-31]
-        self.initial_date = split_date
-        self.final_date = subset.index[-1]
 
+        # reset the index
         subset = subset.reset_index()
 
+        # Create a TimeSeries object from the subset
         ts = TimeSeries.from_dataframe(subset, time_col='date', value_cols=stocks)
+        # split the data into training and validation
         train, val = ts.split_after(split_date)
+
+        # Set the initial date and final date of the trading period
+        self.initial_date = val.time_index.min()
+        self.final_date = val.time_index.max()
+        self.full_trading_times = val.time_index
+
+        # Set the true prices
         self.set_truth(val.pd_dataframe())
+        # Set the initial prices
         self.set_initial_prices(train.pd_dataframe().iloc[-1])
+        # Set the initial portfolio
         self.create_initial_portfolio()
-
-        covariates = covariates.reindex(new_index, method='ffill')
-
-        covariates.index.name = 'date'
-        if covariates.isna().sum().sum() > 0:
-            covariates = covariates.fillna(method='bfill')
-        covariates = covariates.dropna(axis=1, how='any')
-        cov_names = covariates.columns.tolist()
-        covariates = covariates.reset_index()
-        cov_ts = TimeSeries.from_dataframe(covariates, time_col='date', value_cols=cov_names)
-        cov_train, cov_val = cov_ts.split_after(split_date)
+        # Generate Forecasts
         self.create_forecasts(ts, subset, train, val,
-                               cov_ts, covariates, cov_train, cov_val)
+                              cov_ts)
 
     def create_forecasts(self,
                          time_series, subset, train, val,
-                         cov_ts, cov_df, cov_train, cov_val):
-        # model = NLinearModel(
-        #     input_chunk_length=120,
-        #     output_chunk_length=30,
-        #     n_epochs=100,
-        #     random_state=42
-        # )
-
-        # model = TFTModel(
-        #     input_chunk_length=128,
-        #     output_chunk_length=30,
-        #     n_epochs=100,
-        #     random_state=42,
-        #     add_relative_index=True
-        # )
+                         cov_ts):
         model = NHiTSModel(
-            input_chunk_length=128,
+            input_chunk_length=self.lookback,
             output_chunk_length=30,
-            n_epochs=100,
+            n_epochs=125,
             random_state=42,
             layer_widths=128,
             num_layers=4,
-            num_blocks=3,
+            num_blocks=2,
+            loss_fn=MapeLoss()
         )
+
         model.fit(train, verbose=False, past_covariates=cov_ts)
         predictions = [None] * len(val)
         errors = []
@@ -186,50 +194,67 @@ class Experiment:
             updated_split = subset.index[-31 + i]
             updated_train, updated_val = time_series.split_after(updated_split)
             forward_steps = len(val) - i
-            pred = model.predict(forward_steps, series=updated_train, verbose=False, past_covariates=cov_ts)
+            current_prediction = model.predict(forward_steps,
+                                               series=updated_train,
+                                               verbose=False,
+                                               past_covariates=cov_ts)
+            current_prediction_df = current_prediction.pd_dataframe()
+            # if any element of current_prediction_df is nan, raise an error
+            if current_prediction_df.isna().sum().sum() > 0:
+                raise ExperimentGenerationError("Forecast contains nan values")
             if i == 0:
-                predictions[i] = pred.pd_dataframe()
+                predictions[i] = current_prediction_df
             else:
                 df1 = val[:i].pd_dataframe()
-                df2 = pred.pd_dataframe()
+                df2 = current_prediction_df
                 combined_df = pd.concat([df1, df2])
                 predictions[i] = combined_df
-            errors.append(mape(updated_val, pred))
+            errors.append(mape(updated_val, current_prediction))
         self.errors = errors
         self.forecasts = predictions
         self.average_error = np.mean(errors)
 
     @staticmethod
     def choose_split_date():
-        minimum_date = pd.Timestamp('2004-01-01')
+        minimum_date = pd.Timestamp('2006-01-01')
         maximum_date = pd.Timestamp('2021-12-31')
         random_date = pd.Timestamp(random.randint(minimum_date.value, maximum_date.value))
         random_date = random_date.date()
         return random_date
 
 
-def generate_experiments(prices, covariates, num_experiments, output_dir):
+def generate_experiments(stock_price_df, covariates, num_experiments, output_dir, lookback=128):
     pbar = trange(num_experiments)
     average_errors = []
     for i in pbar:
-        experiment = Experiment(prices, covariates)
-        # check if the output directory exists, if not, create it
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        f_name = f'exp_{experiment.num_stocks}_{experiment.budget}_{int(experiment.initial_portfolio_value)}_{int(experiment.final_portfolio_value)}.pkl'
+        try:
+            experiment = ExperimentInfo(stock_price_df, covariates, lookback)
+            # check if the output directory exists, if not, create it
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            f_name = f'exp_{experiment.num_stocks}_{experiment.budget}' \
+                     f'_{int(experiment.initial_portfolio_value)}' \
+                     f'_{int(experiment.final_portfolio_value)}.pkl'
+            # save the experiment to a pickle file in output_dir
+            with open(os.path.join(output_dir, f_name), 'wb') as f:
+                pickle.dump(experiment, f)
+            average_errors.append(experiment.average_error)
+            pbar.set_description(f"Experiment {i} saved to {f_name}. Error is {experiment.average_error}")
 
-        # save the experiment to a pickle file in output_dir
-        with open(os.path.join(output_dir, f_name), 'wb') as f:
-            pickle.dump(experiment, f)
-        average_errors.append(experiment.average_error)
-        pbar.set_description(f"Experiment {i} saved to {f_name}. Error is {experiment.average_error}")
+        except ExperimentGenerationError as e:
+            pbar.set_description(f"Experiment {i} failed with error {e}.")
+        gc.collect()
     print(f"Average error is {np.mean(average_errors)}")
+
+
 if __name__ == '__main__':
+    print("reading data")
     prices = pd.read_parquet('raw_data/spx_stock_prices.parquet')
-    treasury_rate_files = ['daily-treasury-rates.csv'] + [f"daily-treasury-rates ({i}).csv" for i in range(1,25)]
+    treasury_rate_files = ['daily-treasury-rates.csv'] + [f"daily-treasury-rates ({i}).csv" for i in range(1, 25)]
     rates_df = [pd.read_csv(f"raw_data/{file}", index_col=0) for file in treasury_rate_files]
     rates_df = pd.concat(rates_df)
     rates_df.index = pd.to_datetime(rates_df.index)
     # sort rates_df by date
     rates_df = rates_df.sort_index()
-    generate_experiments(prices, rates_df, 10, 'experiments')
+    print("Start Generation")
+    generate_experiments(prices, rates_df, 1, 'experiments', 256)
