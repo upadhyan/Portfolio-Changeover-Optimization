a2a6a4ad99b2f913eabb20126f596b29b58e9465
Nakul Upadhya <upadhyan@purdue.edu>
2023-04-07 16:41:46 -0400

    Update objects.py

--- objects.py
+++ objects.py
@@ -54,120 +54,6 @@ class OnlineReturnsForecasts:
         self.p_hat = (self.r_hat + 1).cumprod().multiply(self.prices[assets].loc[t])
 
 
-class CustomMultiPeriodOptOld:
-    def __init__(
-            self,
-            trading_times,
-            terminal_weights,
-            asset_prices,
-            fixed_cost,
-            return_forecast,
-            lookahead_periods=None,
-            *args,
-            **kwargs
-    ):
-        """
-        trading_times: list, all times at which get_trades will be called
-        lookahead_periods: int or None. if None uses all remaining periods
-        """
-        # Number of periods to look ahead.
-        self.lookahead_periods = lookahead_periods
-        self.trading_times = trading_times
-        self.terminal_weights = terminal_weights
-        self.prices = asset_prices
-        self.fixed_cost = fixed_cost
-        self.return_forecast = return_forecast
-        # super().__init__(*args, **kwargs)
-
-    def get_trades(self, portfolio, t=dt.datetime.today()):
-        ### GUROBI SETTINGS ###
-        env = gp.Env(empty=True)
-        env.setParam("OutputFlag", False)
-        # env.setParam("TimeLimit",time_limit)
-        env.start()
-        #######################
-
-        assets = portfolio.index[:-1]
-        value = self.prices[assets].loc[t] @ portfolio[assets].T + portfolio[-1]
-        assert value > 0.0
-
-        prob_arr = []
-        z_arr = []
-        cash_arr = []
-        p_arr = []
-
-        p = portfolio.values[:-1]  # portfolio of number of positions
-        cash = portfolio.values[-1]  # cash amount
-
-        F = np.ones(p.shape) * self.fixed_cost
-        M = value * 1e3
-
-        m = gp.Model(env=env)
-
-        # planning_periods = self.lookahead_model.get_periods(t)
-        for tau in self.trading_times[
-                   self.trading_times.index(t): self.trading_times.index(t) + self.lookahead_periods
-                   ]:
-            prices = self.return_forecast.get_prices(t, tau, assets)
-
-            # Define in loop vars
-            # Define Variables
-            z_buy = m.addMVar(p.shape, vtype=GRB.INTEGER)
-            z_sell = m.addMVar(p.shape, vtype=GRB.INTEGER)
-            y_sell = m.addMVar(p.shape, vtype=GRB.BINARY)
-            y_buy = m.addMVar(p.shape, vtype=GRB.BINARY)
-
-            # print(prices @ z_sell)
-
-            # This is the next iteration of portfolio
-            p_next = p + z_buy - z_sell
-            cash_next = prices @ (z_sell - z_buy) - F @ y_sell - F @ y_buy + cash
-
-            ## Trading fees
-            ### if we sell a stock, we pay a trading price
-            m.addConstr(M * (y_sell) >= z_sell)
-
-            ### If we buy a stock, we pay a trading fee
-            m.addConstr(M * (y_buy) >= z_buy)
-
-            # No borrowing
-            m.addConstr(cash_next >= 0)
-
-            # No shorting
-            m.addConstr(p_next >= 0)
-
-            prob_arr.append(prices @ p_next - F @ z_sell - F @ z_buy)
-            cash = cash_next
-            p = p_next
-            # prob_arr.append(m)
-
-            z_arr.append(z_buy - z_sell)
-            cash_arr.append(cash_next)
-            p_arr.append(p_next)
-
-        # Terminal constraint.
-        if self.terminal_weights is not None:
-            # Terminal weights constraints
-            m.addConstr(p_next >= self.terminal_weights.values)
-
-        # Combine all time instances
-        obj = sum(prob_arr)
-        m.setObjective(obj, GRB.MAXIMIZE)
-        m.optimize()
-
-        try:
-            self.p_vals = [p.getValue() for p in p_arr]
-            self.cash_vals = [cash.getValue() for cash in cash_arr]
-            self.z_vals = [z.getValue() for z in z_arr]
-            print(f"Z:{self.z_vals[0]}")
-            print(f"P:{self.p_vals[0]}")
-        except Exception as e:
-            print(e)
-
-        assert (self.p_vals[0] >= 0).all()
-        return pd.Series(index=portfolio.index, data=(np.append(self.z_vals[0], self.cash_vals[0])))
-
-
 class CustomMultiPeriodOpt(TradingPolicy):
     def __init__(self, experiment):
         self.exp = experiment
@@ -205,7 +91,7 @@ class CustomMultiPeriodOpt(TradingPolicy):
         trading_information = self.price_dict[t]
         p_next = None
         m = gp.Model(env=env)
-
+        n_timesteps = len(trading_information.index)
         for time in trading_information.index:
             prices = trading_information.loc[time].values
 
@@ -247,7 +133,12 @@ class CustomMultiPeriodOpt(TradingPolicy):
         obj = sum(prob_arr)
 
         m.setObjective(obj, GRB.MAXIMIZE)
-        print("\t Optimizing...")
+        m.update()
+        # get model constraints
+        print(
+            f"\t Optimizing with {n_timesteps} time steps, "
+            f"{len(m.getConstrs())} constraints, and {len(m.getVars())} variables..."
+        )
         m.optimize()
         print("\t Optimized")
         try:
